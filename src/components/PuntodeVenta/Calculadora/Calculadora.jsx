f393d662ce22bdca2ced7626802d05d50c82f0a4
carlosv3l3z <cvelezechavarria@gmail.com>
2025-03-21 19:21:31 -0500

    gestion de obras y calculadora

--- src/components/PuntodeVenta/Calculadora/Calculadora.jsx
+++ src/components/PuntodeVenta/Calculadora/Calculadora.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { Drawer } from 'antd';
 import Carrusel from "./Carrusel.jsx"
 
-
-
 const Calculadora = ({
     isOpen,
     onClose,
@@ -13,13 +11,10 @@ const Calculadora = ({
     descuento,
 }) => {
     const [input, setInput] = useState("");
+    const [savedValue, setSavedValue] = useState(null);
 
     const handleClick = (value) => {
         setInput((prev) => prev + value);
-    };
-
-    const handleClear = () => {
-        setInput("");
     };
 
     const handleDelete = () => {
@@ -28,7 +23,9 @@ const Calculadora = ({
 
     const handleCalculate = () => {
         try {
-            setInput(eval(input).toString()); // Evalúa la expresión (solo suma y resta)
+            const result = eval(input);
+            setSavedValue(result);
+            setInput(result.toString());
         } catch {
             setInput("Error");
         }
@@ -40,19 +37,17 @@ const Calculadora = ({
             placement="right"
             onClose={onClose}
             open={isOpen}
-            width={462}
+            width={518}
             closable={false}
-            bodyStyle={{ padding: "16px" }}
-            drawerStyle={{ borderRadius: "10px 10px 10px 10px"}}
+            bodyStyle={{ paddingLeft: "40px", paddingRight: "40px", paddingTop: "13px"}}
+            drawerStyle={{ borderRadius: "10px 10px 10px 10px" }}
         >
-            {/* Botón de cierre personalizado */}
             <div style={{ position: 'absolute', top: 20, right: 20, cursor: 'pointer' }} onClick={onClose}>
                 <img src='/svg/PuntodeVenta/cerrarcal.svg' alt="" className="cursor-pointer" />
             </div>
 
             <div className='grid grid-cols-2 gap-4 py-3 h-[100%]'>
-                {/* Columna número 1 */}
-                <div className="flex flex-col gap-4 bg-[#EEE] py-10 px-6 rounded-[10px] h-[350px]">
+                <div className="flex flex-col gap-4 py-10 px-6 rounded-[10px] h-[390px] bg-[url('/public/images/fondocalculadora.svg')] bg-cover bg-center bg-no-repeat">
                     <p className="textos-bold gris-elegancia">Subtotal</p>
                     <p className="h4 negro">${subtotal?.toLocaleString()}</p>
                     <p className="textos-bold gris-elegancia">IVA 19%</p>
@@ -62,10 +57,9 @@ const Calculadora = ({
                     <p className="textos-bold gris-elegancia">Total</p>
                     <p className="h4 negro">${total?.toLocaleString()}</p>
                     <p className="h4 verde-corporativo">Devolver</p>
-                    <img src='/svg/PuntodeVenta/puntoscalculadora.svg' alt="" className='mt-[100%] ml-[-3.5%]' />
+                    {savedValue !== null && <p className="h4 verde-corporativo">${savedValue}</p>}
                 </div>
 
-                {/* Columna número 2 */}
                 <div className="flex flex-col gap-2 h-[350px]">
                     <div className='flex w-full justify-center'>
                         <div className='flex bg-[#00733C] w-auto py-1 px-2 gap-2 rounded-[3px]'>
@@ -74,37 +68,30 @@ const Calculadora = ({
                         </div>
                     </div>
 
-                    {/* Input */}
                     <input
                         type="text"
                         value={input}
                         readOnly
                         placeholder='$1.000.000'
                         className="w-full text-center h3 gris-urbano"
-                    /> 
-                    
-                    {/* Carrusel */}
-                    <Carrusel className='flex flex-row'/>
+                    />
 
-                    {/* Teclado numérico */}
+                    <Carrusel onValueSelect={handleClick} />
+
                     <div className="grid grid-cols-3 gap-2">
                         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
                             <button
                                 key={num}
                                 onClick={() => handleClick(num.toString())}
-                                className="p-4 h2"
+                                className="px-4 py-[1px] h2"
                             >
                                 {num}
                             </button>
                         ))}
-                        <button onClick={() => handleClick("+")} className="negro p-4 h2">
-                            +
-                        </button>
-                        <button onClick={() => handleClick("-")} className="negro p-4 h2">
-                            -
-                        </button>
+                        <button onClick={() => handleClick("+")} className="negro p-4 h2">+</button>
+                        <button onClick={() => handleClick("-")} className="negro p-4 h2">-</button>
                         <button onClick={handleDelete} className="negro p-4">
-                        <img src='/svg/PuntodeVenta/borrarcal.svg' alt="" className="cursor-pointer" />
+                            <img src='/svg/PuntodeVenta/borrarcal.svg' alt="" className="cursor-pointer" />
                         </button>
                     </div>
                     <div className='flex w-full justify-center'>
--- src/components/PuntodeVenta/Calculadora/Carrusel.jsx
+++ src/components/PuntodeVenta/Calculadora/Carrusel.jsx
@@ -10,18 +10,18 @@ function PrArrow(props) {
         <img src="/svg/PuntodeVenta/prcalculadora.svg" alt="Flecha" className="mt-[-4%]" />
       </div>
     );
-  }
-
-function NxArrow(props) {
-const { className, style, onClick } = props;
-return (
-    <div className={className} style={{ ...style, display: "block" }} onClick={onClick}>
-    <img src="/svg/PuntodeVenta/nxcalculadora.svg" alt="Flecha" className="mt-[-4%]" />
-    </div>
-);
 }
 
-function Carrusel() {
+function NxArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div className={className} style={{ ...style, display: "block" }} onClick={onClick}>
+            <img src="/svg/PuntodeVenta/nxcalculadora.svg" alt="Flecha" className="mt-[-4%]" />
+        </div>
+    );
+}
+
+function Carrusel({ onValueSelect }) {
     const settings = {
         dots: false,
         infinite: false,
@@ -32,39 +32,20 @@ function Carrusel() {
         prevArrow: <PrArrow />,
     };
 
+    const valores = [2000, 5000, 10000, 20000, 50000, 100000];
+
     return (
         <div className="slider-calculadora flex-row">
             <Slider {...settings}>
-                    <div className="flex flex-row">
-                        <button className="textos rounded-[3px] text-center gris-urbano bg-[#DADADA] p-1">
-                            $2k
+                {valores.map((valor) => (
+                    <div key={valor} className="flex flex-row">
+                        <button 
+                            className="textos rounded-[3px] text-center gris-urbano bg-[#DADADA] p-1"
+                            onClick={() => onValueSelect(valor.toString())}>
+                            ${valor / 1000}k
                         </button>
                     </div>
-                    <div className="flex flex-row">
-                        <button className="textos rounded-[3px] text-center gris-urbano bg-[#DADADA] p-1">
-                            $5k
-                        </button>
-                    </div>
-                    <div className="flex flex-row">
-                        <button className="textos rounded-[3px] text-center gris-urbano bg-[#DADADA] p-1">
-                            $10k
-                        </button>
-                    </div>
-                    <div className="flex flex-row">
-                        <button className="textos rounded-[3px] text-center gris-urbano bg-[#DADADA] p-1">
-                            $20k
-                        </button>
-                    </div>
-                    <div className="flex flex-row">
-                        <button className="textos rounded-[3px] text-center gris-urbano bg-[#DADADA] p-1">
-                            $50k
-                        </button>
-                    </div>
-                    <div className="flex flex-row">
-                        <button className="textos rounded-[3px] text-center gris-urbano bg-[#DADADA] p-1">
-                            $100k
-                        </button>
-                    </div>
+                ))}
             </Slider>
         </div>
     );
